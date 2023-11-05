40ade335516111cc4a31da31143d888b91ec4658
ilya2409 <73117875+ilya2409@users.noreply.github.com>
2023-11-05 23:15:55 +0300

    Update mini_form.py

--- UVBB-BOT/mini_form.py
+++ UVBB-BOT/mini_form.py
@@ -1,26 +1,53 @@
-from python_files.test_editor import add_2
-
-bumashka_data = "| none |"
+# from python_files.test_editor import add_2, dell_2, search_2
+from python_files.editor_2 import create, dell_2, search_2
 
 mini_key = "qwerty-off"
-
-
-def mini_latter(latter_text):
-    with open('hello.py') as latter_a:
-        latter_a = sum(1 for _ in latter_a)
-        print(latter_a)
-
-    with open("hello.py", "a") as file_a:
-        file_a.write(f"latter_number{latter_a + 1} = '{latter_text}'" + "\n")
+new_type = "new_type"
+new_header = "new_header"
+new_content = "new_content"
+new_author = "new_author"
 
 
 def latter_prog(text_a):
     text_b = text_a.split("\n")
-    print(text_b)
-    if len(text_b) > 2:
-        author_b = text_b[2]
+    global new_type, new_header, new_content, new_author
+    print("\n")
+    print(f"text_b: {text_b}")
+
+    new_type = text_b[0].split()[0]
+    new_header = text_b[0][2:-1]
+    new_content = text_b[1]
+    new_author = text_b[2]
+
+    print(f"new_header: {new_header}")
+
+    if new_type == "#comment":
+        create("txt", new_header, new_content, new_author)
+
+    elif new_type == "#image":
+        create("img", new_header, new_content, new_author)
+
+    elif "#delete" in text_a:
+        com = search_2(text_a.split()[1])
+        print(f"serch_2.com: {com}")
+        dell_2(com)
+
     else:
-        author_b = "Anonim)"
-    tema = text_b[1]
-    tip_b = "txt"
-    add_2(tip_b, tema, author_b)
+        create("txt", new_header, new_content, new_author)
+
+
+def mini_latter(latter_text):
+    global new_header
+
+    with open('hello.py') as latter_a:
+        latter_a = sum(1 for _ in latter_a)
+        print(f"latter_a: {latter_a}")
+    latter_out = latter_text.split("\n")
+
+    with open("hello.py", "a") as file_a:
+        file_a.write(f"""latter_{new_header} = {latter_out} """ + "\n")
+        # file_a.write(f'latter_number{latter_a + 1} = """{latter_text}"""' + "\n")
+
+
+# latter_out = "latter result"
+# latter_result = latter_out.split("\n")
